88efc83d9372f0475cda6ad663194607751f5c68
Ward D <w@rddeboutte.com>
2025-09-11 20:40:57 +0200

    feat: meth file support for methyldackel, allcools, bismark cov and bismark CpG. Both gz and regular. (#32)
    docs: includes docs update for filetypes
    test: pytests include default test for allcools, bismark cov, methyldackel and CpG report files.

--- python/tests/conftest.py
+++ python/tests/conftest.py
@@ -24,46 +24,180 @@ Also includes some helper function to parse mudata objects.
 '''
 
 @pytest.fixture
-def meth_path(tmp_path):
+def dynamic_methylation_path(request):
+    '''
+    helps to return fixture to parametrize over.
+    '''
+    return request.getfixturevalue(request.param)
+
+@pytest.fixture
+def allcools_path(tmp_path):
     '''
     simulate allcools.tsv.gz files
     tsv with columns chr, pos, strand, context, meth, cov and sign.
     sign denotes if significantly methylated (1 = no test).
+    1-based
     '''
-    methpath = tmp_path / "methylation"
+    methpath = tmp_path / "allcools"
     methpath.mkdir()
 
     # Cell 1 - avg. meth in gene 1 = 50%
     with gzip.open(methpath / "cell1.WCGN.tsv.gz", "wt") as f:
-        f.write('chr1\t20\t+\tTCGG\t25\t100\t1\n')
-        f.write('chr1\t50\t+\tACGT\t75\t100\t1\n')
+        f.write('chr1\t21\t+\tTCGG\t25\t100\t1\n')
+        f.write('chr1\t51\t+\tACGT\t75\t100\t1\n')
 
     # Cell 2 - avg. meth in gene 2 = 62.5%
     with gzip.open(methpath / "cell2.WCGN.tsv.gz", "wt") as f:
-        f.write('chr1\t221\t+\tACGA\t2\t4\t1\n')
-        f.write('chr1\t244\t+\tACGA\t12\t16\t1\n')
+        f.write('chr1\t222\t+\tACGA\t2\t4\t1\n')
+        f.write('chr1\t245\t+\tACGA\t12\t16\t1\n')
     
     # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
     with gzip.open(methpath / "cell3.WCGN.tsv.gz", "wt") as f:
-        f.write('chr1\t20\t+\tTCGG\t21\t21\t1\n')
-        f.write('chr1\t50\t+\tACGT\t35\t35\t1\n')
-        f.write('chr1\t221\t+\tACGA\t0\t100\t1\n')
+        f.write('chr1\t21\t+\tTCGG\t21\t21\t1\n')
+        f.write('chr1\t51\t+\tACGT\t35\t35\t1\n')
+        f.write('chr1\t222\t+\tACGA\t0\t100\t1\n')
     
     # Cell 1 - avg. meth in gene 1 = 25%
     with gzip.open(methpath / "cell1.GCHN.tsv.gz", "wt") as f:
-        f.write('chr1\t30\t+\tGCAA\t25\t100\t1\n')
-        f.write('chr1\t40\t+\tGCTC\t1\t4\t1\n')
+        f.write('chr1\t31\t+\tGCAA\t25\t100\t1\n')
+        f.write('chr1\t41\t+\tGCTC\t1\t4\t1\n')
 
     # Cell 2 - avg. meth in gene 2 = 44%
     with gzip.open(methpath / "cell2.GCHN.tsv.gz", "wt") as f:
-        f.write('chr1\t230\t+\tGCCC\t44\t100\t1\n')
+        f.write('chr1\t231\t+\tGCCC\t44\t100\t1\n')
     
     # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
     with gzip.open(methpath / "cell3.GCHN.tsv.gz", "wt") as f:
-        f.write('chr1\t30\t+\tGCAA\t100\t100\t1\n')
-        f.write('chr1\t40\t+\tGCTC\t4\t4\t1\n')
-        f.write('chr1\t230\t+\tGCCC\t0\t100\t1\n')
+        f.write('chr1\t31\t+\tGCAA\t100\t100\t1\n')
+        f.write('chr1\t41\t+\tGCTC\t4\t4\t1\n')
+        f.write('chr1\t231\t+\tGCCC\t0\t100\t1\n')
 
+    return methpath
+
+@pytest.fixture
+def methyldackel_path(tmp_path):
+    '''
+    simulate methyldackel files
+    tsv with columns chrom, start, end, perc, methylcount, coverage
+    0-based
+    '''
+    methpath = tmp_path / "methyldackel"
+    methpath.mkdir()
+
+    # Cell 1 - avg. meth in gene 1 = 50%
+    with open(methpath / "cell1.WCGN.bedgraph", "w") as f:
+        f.write('chr1\t20\t21\t25\t25\t100\n')
+        f.write('chr1\t50\t51\t75\t75\t100\n')
+
+    # Cell 2 - avg. meth in gene 2 = 62.5%
+    with open(methpath / "cell2.WCGN.bedgraph", "w") as f:
+        f.write('chr1\t221\t222\t50\t2\t4\n')
+        f.write('chr1\t244\t245\t75\t12\t16\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with open(methpath / "cell3.WCGN.bedgraph", "w") as f:
+        f.write('chr1\t20\t21\t100\t21\t21\n')
+        f.write('chr1\t50\t51\t100\t35\t35\n')
+        f.write('chr1\t221\t222\t0\t0\t100\n')
+    
+    # Cell 1 - avg. meth in gene 1 = 25%
+    with open(methpath / "cell1.GCHN.bedgraph", "w") as f:
+        f.write('chr1\t30\t31\t25\t25\t100\n')
+        f.write('chr1\t40\t41\t25\t1\t4\n')
+
+    # Cell 2 - avg. meth in gene 2 = 44%
+    with open(methpath / "cell2.GCHN.bedgraph", "w") as f:
+        f.write('chr1\t230\t231\t44\t44\t100\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with open(methpath / "cell3.GCHN.bedgraph", "w") as f:
+        f.write('chr1\t30\t31\t100\t100\t100\n')
+        f.write('chr1\t40\t41\t100\t4\t4\n')
+        f.write('chr1\t230\t231\t0\t0\t100\n')
+    return methpath
+
+@pytest.fixture
+def bismarkcov_path(tmp_path):
+    '''
+    simulate bismark coverage files
+    tsv with columns chrom, start, end, perc, methylcount, non methylcount
+    these are 1-based.
+    '''
+    methpath = tmp_path / "bismarkcov"
+    methpath.mkdir()
+
+    # Cell 1 - avg. meth in gene 1 = 50%
+    with gzip.open(methpath / "cell1.WCGN.bismark.cov", "wt") as f:
+        f.write('chr1\t21\t22\t25\t25\t75\n')
+        f.write('chr1\t51\t52\t75\t75\t25\n')
+
+    # Cell 2 - avg. meth in gene 2 = 62.5%
+    with gzip.open(methpath / "cell2.WCGN.bismark.cov", "wt") as f:
+        f.write('chr1\t222\t223\t50\t2\t2\n')
+        f.write('chr1\t245\t246\t75\t12\t4\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with gzip.open(methpath / "cell3.WCGN.bismark.cov", "wt") as f:
+        f.write('chr1\t21\t22\t100\t21\t0\n')
+        f.write('chr1\t51\t52\t100\t35\t0\n')
+        f.write('chr1\t222\t223\t0\t0\t100\n')
+    
+    # Cell 1 - avg. meth in gene 1 = 25%
+    with gzip.open(methpath / "cell1.GCHN.bismark.cov", "wt") as f:
+        f.write('chr1\t31\t32\t25\t25\t75\n')
+        f.write('chr1\t41\t42\t25\t1\t3\n')
+
+    # Cell 2 - avg. meth in gene 2 = 44%
+    with gzip.open(methpath / "cell2.GCHN.bismark.cov", "wt") as f:
+        f.write('chr1\t231\t232\t44\t44\t56\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with gzip.open(methpath / "cell3.GCHN.bismark.cov", "wt") as f:
+        f.write('chr1\t31\t32\t100\t100\t0\n')
+        f.write('chr1\t41\t42\t100\t4\t0\n')
+        f.write('chr1\t231\t232\t0\t0\t100\n')
+    return methpath
+
+@pytest.fixture
+def bismarkrep_path(tmp_path):
+    '''
+    simulate bismark CpG report files
+    tsv with chrom, pos, strand, methylcount, non methylcount, c-context, trinucleotide context
+    these are 0-based.
+    '''
+    methpath = tmp_path / "bismarkrep"
+    methpath.mkdir()
+
+    # Cell 1 - avg. meth in gene 1 = 50%
+    with gzip.open(methpath / "cell1.WCGN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t20\t+\t25\t75\tCG\tCGN\n')
+        f.write('chr1\t50\t+\t75\t25\tCG\tCGN\n')
+
+    # Cell 2 - avg. meth in gene 2 = 62.5%
+    with gzip.open(methpath / "cell2.WCGN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t221\t+\t2\t2\tCG\tCGN\n')
+        f.write('chr1\t244\t+\t12\t4\tCG\tCGN\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with gzip.open(methpath / "cell3.WCGN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t20\t+\t21\t0\tCG\tCGN\n')
+        f.write('chr1\t50\t+\t35\t0\tCG\tCGN\n')
+        f.write('chr1\t221\t+\t0\t100\tCG\tCGN\n')
+    
+    # Cell 1 - avg. meth in gene 1 = 25%
+    with gzip.open(methpath / "cell1.GCHN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t30\t+\t25\t75\tCG\tCGN\n')
+        f.write('chr1\t40\t+\t1\t3\tCG\tCGN\n')
+
+    # Cell 2 - avg. meth in gene 2 = 44%
+    with gzip.open(methpath / "cell2.GCHN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t230\t+\t44\t56\tCG\tCGN\n')
+    
+    # Cell 3 - avg. meth in gene 1 = 100%, avg. meth in gene 2 = 0%
+    with gzip.open(methpath / "cell3.GCHN.bismark.rep.gz", "wt") as f:
+        f.write('chr1\t30\t+\t100\t0\tCG\tCGN\n')
+        f.write('chr1\t40\t+\t4\t0\tCG\tCGN\n')
+        f.write('chr1\t230\t+\t0\t100\tCG\tCGN\n')
     return methpath
 
 @pytest.fixture
